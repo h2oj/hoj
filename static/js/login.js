d2567be4f5c04a3b3da385d10aac3d25ff7cc0f3
Alex Cui <cuizhihui030925@outlook.com>
2019-09-22 17:16:57 +0800

    frontend: login/sign up page

---
+++ static/js/login.js
@@ -0,0 +1,17 @@
+function select(ele) {
+    let p = ele.parentElement;
+    if (p.firstElementChild.className == 'login-tab-select') {
+        p.lastElementChild.className = 'login-tab-select';
+        p.firstElementChild.className = '';
+        document.getElementById('login-login').hidden = true;
+        document.getElementById('login-signup').hidden = false;
+    }
+    else {
+        p.firstElementChild.className = 'login-tab-select';
+        p.lastElementChild.className = '';
+        document.getElementById('login-signup').hidden = true;
+        document.getElementById('login-login').hidden = false;
+    }
+    return 0;
+}
+
