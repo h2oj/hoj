cf9b63566edead656832c0ee4ae26bf2c435f5a2
Alex Cui <cuizhihui030925@outlook.com>
2019-11-30 18:52:45 +0800

    now ide can change language by change the selection

---
+++ static/js/idelang.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+let IDELanguage = {
+    'cpp': 'cpp',
+    'cpp98': 'cpp',
+    'cpp03': 'cpp',
+    'cpp11': 'cpp',
+    'cpp14': 'cpp',
+    'cpp17': 'cpp',
+    'cpp20': 'cpp',
+    'c': 'c',
+    'c89': 'c89',
+    'c99': 'c99',
+    'c11': 'c11',
+    'c18': 'c18',
+    'java': 'java',
+    'js': 'javascript',
+    'scratch': 'json'
+};
+
