d540c8a4e2fa3c2428a8632b040cf699c752e65a
Alex Cui <cuizhihui030925@outlook.com>
2020-05-21 23:27:13 +0800

    submission update and fixed xhr bugs

--- src/routes/submission.js
+++ src/routes/submission.js
@@ -4,17 +4,23 @@ const express = require('express');
 
 const Submission = require('../models-build/submission').default;
 const TestPoint = require('../models-build/test_point').default;
+const User = require('../models-build/user').default;
+const Problem = require('../models-build/problem').default;
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    let page = req.body.page || 1;
-    let each = req.body.each || 5;
-    let submission = await Submission.find({
+    let page = req.query.page || 1;
+    let each = req.query.each || 5;
+    let submissions = await Submission.find({
         skip: (page - 1) * each,
         take: each
     });
-    res.render('submissionlist.pug', { submission: submission });
+    for (let submission of submissions) {
+        submission.user = await User.fromUid(submission.uid);
+        submission.problem = await Problem.fromPid(submission.pid);
+    }
+    res.render('submissionlist.pug', { submissions: submissions });
 });
 
 router.get('/:sid', async (req, res) => {
