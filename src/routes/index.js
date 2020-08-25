6ff8ec5b6199c26eb06bc7b6f5a200280d8230bf
sparrowhe <m15562283768@163.com>
2020-08-25 17:22:45 +0800

    Add username in topbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,17 @@
 
 const express = require('express');
 
+const User = require('../models-build/user').default;
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index.pug');
+router.get('/', async (req, res) => {
+    if (req.session.user_id) {
+        const user = await User.fromUid(req.session.user_id);
+        res.render('index.pug', { user: user });
+    }else {
+        res.render('index.pug');
+    }
 });
 
 module.exports = router;
